8d6ef5e22ad26f1bb32284f731cceaf00a94c080
devjosegomez <devjosegomez@gmail.com>
2019-10-17 15:36:21 -0500

    Implemented getters and setters base & height

--- src/com/exercise12oop/model/Triangle.java
+++ src/com/exercise12oop/model/Triangle.java
@@ -1,5 +1,35 @@
 package com.exercise12oop.model;
 
 public class Triangle extends Shape{
+	private double base;
+	private double height;
 	
+	public Triangle() {
+		
+	}
+	
+	public Triangle(double base, double height) {
+		this.base = base;
+		this.height = height;
+	}
+	
+	//getters and setters Base
+	
+	public double getBase() {
+		return base;
+	}
+	
+	public void setBase(double base) {
+		this.base = base;
+	}
+	
+	
+	//getters and setters Height
+	public double getHeight() {
+		return height;
+	}
+	
+	public void setHeight(double height) {
+		this.height = height;
+	}
 }
