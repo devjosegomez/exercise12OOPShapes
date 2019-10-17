7e6385f9d69f8acf7a62ebd4af14bd718fdf8060
devjosegomez <devjosegomez@gmail.com>
2019-10-17 15:22:09 -0500

    Initial commit

---
+++ src/com/exercise12oop/model/Shapeable.java
@@ -0,0 +1,9 @@
+package com.exercise12oop.model;
+
+public interface Shapeable {
+	public void Draw();
+	public void ChangeColor();
+	public void Erase();
+	public void CalculateArea(double b, double h);
+	public void CalculatePerimeter();
+}
