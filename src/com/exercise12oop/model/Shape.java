8b4707253d86fa7d75e34ecf568b954b608826e7
devjosegomez <devjosegomez@gmail.com>
2019-10-17 15:24:31 -0500

    Added Shape class, interface Shepable

---
+++ src/com/exercise12oop/model/Shape.java
@@ -0,0 +1,35 @@
+package com.exercise12oop.model;
+
+public class Shape implements Shapeable {
+
+	@Override
+	public void Draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ChangeColor() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Erase() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void CalculateArea(double b, double h) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void CalculatePerimeter() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
